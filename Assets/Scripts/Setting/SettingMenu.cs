aad2f25c430814d2ed632bc8627949084141ea85
Dragon <nawatng@gmail.com>
2024-01-19 10:53:33 +0700

    - Setting Panal (Audio setting, Resolution and Graphics quality)
    Audio setting has 3 sub channal (Master, SFX and Background), Graphics have 5 level and Resolution scale 10+

---
+++ Assets/Scripts/Setting/SettingMenu.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    [Header("Audio Settings")]
+    [SerializeField] AudioMixer AudioMixer;
+
+    [Header("Resolution Settings")]
+    [SerializeField] Resolution [] Resolution;
+    [SerializeField] TMPro.TMP_Dropdown ResolutionDropdown;
+    [SerializeField] GameObject fpsText;
+    [SerializeField] bool ShowfpsText = false;
+    float deltaTime;
+
+    private void Start()
+    {
+        Resolution = Screen.resolutions; //Put all aviable resolutuion in this variable
+        ResolutionDropdown.ClearOptions (); //Clear Previous one
+
+        List<string> Alloption = new List<string> ();
+
+        int currentResoultionIndex = 0;
+
+        for(int i = 0; i < Resolution.Length; i++)
+        {
+            string options = Resolution[i].width + " x " + Resolution[i].height;
+            Alloption.Add(options);
+
+            if (Resolution[i].width == Screen.currentResolution.width && Resolution[i].height == Screen.currentResolution.height)
+            {
+                currentResoultionIndex = i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(Alloption);
+        ResolutionDropdown.value = currentResoultionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = Resolution[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetMasterVolume(float volume)
+    {
+        AudioMixer.SetFloat("Master", volume);        
+    }
+
+    public void SetBGVolume(float volume)
+    {
+        AudioMixer.SetFloat("Background", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        AudioMixer.SetFloat("SFX", volume);
+    }
+
+    public void SetQuality(int level) 
+    { 
+        QualitySettings.SetQualityLevel(level);
+    }
+
+    public void ToggleFPS()
+    {
+        if (!ShowfpsText)
+        {
+            ShowfpsText = true;
+            fpsText.SetActive(true);
+            
+        }
+        else if (ShowfpsText)
+        {
+            ShowfpsText = false;
+            fpsText.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        if(ShowfpsText)
+        {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+            float fps = 1.0f / deltaTime;            
+            fpsText.GetComponent<TextMeshProUGUI>().text = "FPS: " + Mathf.Ceil(fps).ToString();
+        }  
+    }
+}
